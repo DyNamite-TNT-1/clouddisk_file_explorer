4e5a810147a5823209e0a7d0cdb35f4c58d68238
DESKTOP-HFM071Q\Duc <lemauanhduc@hanbiro.vn>
2022-12-16 13:47:36 +0700

    add singlechildscrollview in login screen

--- lib/presentation/pages/login_page/login_screen.dart
+++ lib/presentation/pages/login_page/login_screen.dart
@@ -69,82 +69,85 @@ class _LoginScreenState extends State<LoginScreen> {
       },
       child: BlocBuilder<LoginBloc, LoginState>(
         builder: (context, state) {
-          return Container(
-            decoration: const BoxDecoration(color: Colors.lightBlue),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(32, 86, 32, 0),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 1,
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.lightBlue),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(32, 86, 32, 0),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 1,
+                          ),
+                        ),
+                        child: const Text(
+                          'HANBIRO',
+                          style: TextStyle(
+                            fontSize: 12,
+                            letterSpacing: 20.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                      child: const Text(
-                        'HANBIRO',
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const Text(
+                        'Cloud Disk',
                         style: TextStyle(
-                          fontSize: 12,
-                          letterSpacing: 20.0,
+                          fontSize: 48,
+                          letterSpacing: 2.0,
                           color: Colors.white,
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    const Text(
-                      'Cloud Disk',
-                      style: TextStyle(
-                        fontSize: 48,
-                        letterSpacing: 2.0,
-                        color: Colors.white,
+                      const SizedBox(
+                        height: 36,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 36,
-                    ),
-                    AuthInput(
-                      domainController: domainController,
-                      label: "Domain",
-                      icon: Icons.language,
-                      keyboardType: TextInputType.name,
-                      isPassword: false,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    AuthInput(
-                      domainController: idController,
-                      label: "ID",
-                      icon: Icons.person,
-                      keyboardType: TextInputType.name,
-                      isPassword: false,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    AuthInput(
-                      domainController: passwordController,
-                      label: "Password",
-                      icon: Icons.lock,
-                      keyboardType: TextInputType.visiblePassword,
-                      isPassword: true,
-                    ),
-                    const SizedBox(
-                      height: 56,
-                    ),
-                    AuthButton(
-                      label: "Log In",
-                      onTap: () {
-                        BlocProvider.of<LoginBloc>(context)
-                            .add(ClickedLoginEvent(logInRequest));
-                      },
-                    ),
-                  ],
+                      AuthInput(
+                        domainController: domainController,
+                        label: "Domain",
+                        icon: Icons.language,
+                        keyboardType: TextInputType.name,
+                        isPassword: false,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      AuthInput(
+                        domainController: idController,
+                        label: "ID",
+                        icon: Icons.person,
+                        keyboardType: TextInputType.name,
+                        isPassword: false,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      AuthInput(
+                        domainController: passwordController,
+                        label: "Password",
+                        icon: Icons.lock,
+                        keyboardType: TextInputType.visiblePassword,
+                        isPassword: true,
+                      ),
+                      const SizedBox(
+                        height: 56,
+                      ),
+                      AuthButton(
+                        label: "Log In",
+                        onTap: () {
+                          BlocProvider.of<LoginBloc>(context)
+                              .add(ClickedLoginEvent(logInRequest));
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
